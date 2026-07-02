32a7d0135d21892dd3118fb56486dbd1db8508d7
ShuKhi22 <shubhamkhichi22@gmail.com>
2026-07-02 14:57:02 +0000

    Update kissing fast results

--- kissing-fast.py
+++ kissing-fast.py
@@ -854,7 +854,7 @@ def train_with_LBFGS(
 if __name__ == "__main__":
     use_GPU = True
     learning_rate = 1e-2
-    max_iter = 2000
+    max_iter = 3000
     mu = 0.1
 
     Gq, Gt, n = read_file()
